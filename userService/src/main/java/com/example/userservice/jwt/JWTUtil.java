8bbc17935b4fab644be9de954495c6cb97aafdcf
JungWook87 <119837276+JungWook87@users.noreply.github.com>
2024-02-29 17:23:10 +0900

    jwt토큰 검증및생성

---
+++ userService/src/main/java/com/example/userservice/jwt/JWTUtil.java
@@ -0,0 +1,58 @@
+package com.example.userservice.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@PropertySource("classpath:jwt.yml") // 소스파일 지정
+@Component
+public class JWTUtil {
+
+    private final Key key;
+    private final String issuer;
+
+    public JWTUtil(@Value("${secret-key}")String secret,
+                   @Value("${issuer}")String issuer){
+        byte[] byteSecretKey = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(byteSecretKey);
+        this.issuer = issuer;
+    }
+
+    public String getUsername(String token){
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJwt(token).getBody().get("username", String.class);
+    }
+
+    public String getRole(String token){
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJwt(token).getBody().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token){
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJwt(token).getBody().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs){
+        Claims claims = Jwts.claims();
+        claims.put("username", username);
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setIssuer(issuer)
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(key, SignatureAlgorithm.ES512)
+                .compact();
+    }
+
+}
